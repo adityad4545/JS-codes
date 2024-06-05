d991f1bc0513c7c715562a502bcf01ce522eb324
adityad4545 <durapheaditya92@gmail.com>
2024-06-05 14:18:49 +0000

    Datatypes-Summary

---
+++ .devcontainer/01_Basics/04_comparison.js
@@ -0,0 +1,6 @@
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//the difference between '==' and '===' is '==' only compares the values and '===' compares both values and their datatypes
+console.log("2"===2);
---
+++ .devcontainer/01_Basics/05_datatypes-summary.js
@@ -0,0 +1,47 @@
+/*1> primitive datatypes = > String,Number,bigInt,Boolean,Undefined,Null,Symbol
+  2> Non-primitive = > object,function,array 
+
+  javascript is the dynamically typed language beacause it allows variables to change their datatype at a runtime*/
+  // let name1=Symbol('aditya');
+  // let name2=Symbol('aditya');
+  // console.log(name1===name2);
+  // // 1> Arrays
+
+  // let myArr=["rohit sharma","virat kohli","jasprit bumrah"];
+  // console.log(myArr);
+
+  // //2> Objects
+
+  // const playerInfo={
+  //   name1 : 'Rohit Sharma',
+  //   age : 34,
+  //   Team : 'Indian Cricket Team'
+  // }
+  // console.log(playerInfo)
+
+  // //3>functions
+  
+  // let myFunction=function(){
+  //    console.log("hello world")
+  //  }
+  // // console.log(myFunction)
+  // console.log(typeof(myFunction));
+
+
+/* output of checking datatype using the typeOf function as follows = >
+
+Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+*/
