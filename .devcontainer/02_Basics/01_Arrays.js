e10604a411855828de11110c48df072888e17b19
adityad4545 <durapheaditya92@gmail.com>
2024-06-11 05:38:43 +0000

    push opartion

---
+++ .devcontainer/02_Basics/01_Arrays.js
@@ -0,0 +1,10 @@
+const myArr=[1,2,3,4,5];
+console.log(myArr);
+const myArr2 = new Array("abc", "xyz", "pqr");
+myArr2.push("ajay");
+console.log(myArr2);
+myArr2.pop()
+console.log(myArr2);
+myArr2.unshift("ajit");
+console.log(myArr2);
+console.log("hii gihub i want check the push operation");
